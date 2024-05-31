ad4e7a30deab85122bc6bc53b8f058262e40c50d
Sakshi Pandey <sakshirampravesh1@gmail.com>
2024-05-31 17:44:27 +0530

    error boundaries in react

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      error: null,
+      errorInfo: null
+    };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    this.setState({
+      hasError: true,
+      error: error,
+      errorInfo: errorInfo
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h1>Something went wrong...</h1>
+          <p>{this.state.error && this.state.error.toString()}</p>
+          <p>{this.state.errorInfo && this.state.errorInfo.componentStack}</p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
---
+++ src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Error boundary task</h1>
+      <p> Error boundaries are React components that implement either componentDidCatch lifecycle method or the static getDerivedStateFromError method. These methods are used to catch errors that occur during rendering, in lifecycle methods, and in constructors of the whole tree below them.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
